b4a452f66cae3a4815f4be5079d0992c6f385d6e
Jared Poston <postonjw@miamioh.edu>
2018-02-11 18:46:44 -0500

    added +=, -=, print

--- HW1/src/Main.java
+++ HW1/src/Main.java
@@ -53,22 +53,36 @@ public class Main {
 				else if (arr[i].equals("*=")) {
 					
 					if (map.containsKey(arr[i-1]) && map.get(arr[i-1])[1].equals("Int")) {
+						
 						try {
-							if (arr[i+1].matches("-\\d+")) { // arg on right side of *= is an integer
+							
+							if (arr[i+1].matches("-?\\d+")) { // arg on right side of *= is an integer
 								int newVal = Integer.parseInt(map.get(arr[i-1])[0]) * Integer.parseInt(arr[i+1]);
 								map.replace(arr[i-1], new String[] {Integer.toString(newVal), "Int"});
 							}
+							
 							else if (map.containsKey(arr[i+1])) { // arg on right side of *= is another variable
-								int rightVal = Integer.parseInt(map.get([arr+1])[0]);
+								int rightVal = -1;
+								try {
+									rightVal = Integer.parseInt(map.get(arr[i+1])[0]);
+								}
+								
+								catch (NumberFormatException e) { // runtime error occurred, exiting
+									System.out.println("Error on line " + lineNum);
+									System.exit(0);
+								}
+								
 								int newVal = Integer.parseInt(map.get(arr[i-1])[0]) * rightVal;
 								map.replace(arr[i-1], new String[] {Integer.toString(newVal), "Int"});
 							}
-							else {
+							
+							else { // runtime error occurred, exiting
 								System.out.println("Error on line " + lineNum);
 								System.exit(0);
 							}
 						}
-						catch (NumberFormatException e) {
+						
+						catch (NumberFormatException e) { // runtime error occurred, exiting
 							System.out.println("Error on line " + lineNum);
 							System.exit(0);
 						}
@@ -78,18 +92,122 @@ public class Main {
 				else if (arr[i].equals("+=")) {
 					
 					if (map.containsKey(arr[i-1]) && map.get(arr[i-1])[1].equals("Int")) {
+						try {
+							
+							if (arr[i+1].matches("-?\\d+")) { // arg on right side of += is an integer
+								int newVal = Integer.parseInt(map.get(arr[i-1])[0]) + Integer.parseInt(arr[i+1]);
+								map.replace(arr[i-1], new String[] {Integer.toString(newVal), "Int"});
+							}
+							
+							else if (map.containsKey(arr[i+1])) { // arg on right side of += is a variable
+								int rightVal = -1;
+								
+								try {
+									rightVal = Integer.parseInt(map.get(arr[i+1])[0]);
+								}
+								
+								catch (NumberFormatException e) { // runtime error occurred, exiting
+									System.out.println("Error on line " + lineNum);
+									System.exit(0);
+								}
+								
+								int newVal = Integer.parseInt(map.get(arr[i-1])[0]) + rightVal;
+								map.replace(arr[i-1], new String[] {Integer.toString(newVal), "Int"});
+							}
+						}
 						
+						catch (NumberFormatException e) {
+							System.out.println("Error on line " + lineNum);
+							System.exit(0);
+						}
 					}
-					else if (map.containsKey(arr[i-1]) && map.get(arr[i-1])[1].equals("String")) {
+					
+					else if (map.containsKey(arr[i-1]) && map.get(arr[i-1])[1].equals("String")) { // operating on a string
+						String newString = "";
+						if (arr[i+1].contains("\"")) { // arg on right of += is a literal string
+							
+							newString += (map.get(arr[i-1])[0].replaceAll("\"", ""));
+							newString += (arr[i+1].replaceAll("\"", ""));
+							
+							map.replace(arr[i-1], new String[] {newString, "String"});
+						}
 						
+						else if (map.containsKey(arr[i+1]) && map.get(arr[i+1])[1].equals("String")) { // arg on right of += is a variable of type string
+							
+							newString += (map.get(arr[i-1])[0].replaceAll("\"", " "));
+							newString += (map.get(arr[i+1])[0].replaceAll("\"", ""));
+							
+							map.replace(arr[i-1], new String[] {newString, "String"});
+						}
+						
+						else { // invalid right side
+							System.out.println("Error on line " + lineNum);
+							System.exit(0);
+						}
+					}
+					
+					else { // left side arg is invalid
+						System.out.println("Error on line " + lineNum);
+						System.exit(0);
+					}
+				}
+				
+				else if (arr[i].equals("-=")) {
+					
+					if (map.containsKey(arr[i-1]) && map.get(arr[i-1])[1].equals("Int")) {
+						
+						try {
+							
+							if (arr[i+1].matches("-?\\d+")) { // arg on right side of -= is an integer
+								int newVal = Integer.parseInt(map.get(arr[i-1])[0]) - Integer.parseInt(arr[i+1]);
+								map.replace(arr[i-1], new String[] {Integer.toString(newVal), "Int"});
+							}
+							
+							else if (map.containsKey(arr[i+1])) { // arg on right side of -= is another variable
+								int rightVal = -1;
+								try {
+									rightVal = Integer.parseInt(map.get(arr[i+1])[0]);
+								}
+								
+								catch (NumberFormatException e) { // runtime error occurred, exiting
+									System.out.println("Error on line " + lineNum);
+									System.exit(0);
+								}
+								
+								int newVal = Integer.parseInt(map.get(arr[i-1])[0]) - rightVal;
+								map.replace(arr[i-1], new String[] {Integer.toString(newVal), "Int"});
+							}
+							
+							else { // runtime error occurred, exiting
+								System.out.println("Error on line " + lineNum);
+								System.exit(0);
+							}
+						}
+						
+						catch (NumberFormatException e) { // runtime error occurred, exiting
+							System.out.println("Error on line " + lineNum);
+							System.exit(0);
+						}
+					}
+				}
+				
+				else if (arr[i].equals("FOR")) { // handle for loops
+					String[] toEndFor = arr; // store entire line for parsing
+					
+					
+				}
+				
+				else if (arr[i].equals("PRINT")) {
+					if (map.get(arr[i+1])[1].equals("String")) {
+						System.out.println(arr[i+1] + " = " + map.get(arr[i+1])[0].replaceAll("\"", ""));
 					}
 					else {
-						
+						System.out.println(arr[i+1] + " = " + map.get(arr[i+1])[0]);
 					}
+					
 				}
 			}
 		}
-		
 		
 		
 		in.close();
